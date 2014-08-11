cfad3167d18e72c03e2061fb7a32aee7c9d45daa
laudbrian <laudbrian@gmail.com>
2014-08-11 00:10:30 -0700

    added Angular  objects, controllers, and HTML5 audio element

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 // Define Angular module - collection of functions ran when app is booted
 
 var app = angular.module('myApp', []);
+
+
+
+app.controller('PlayerController', ['$scope', function($scope) {
+  $scope.playing = false;
+  $scope.audio = document.createElement('audio');
+  $scope.audio.src = 'http://linode.rabasa.com/cantina.mp3';
+  $scope.play = function() {
+    $scope.audio.play();
+    $scope.playing = true;
+  };
+  $scope.stop = function() {
+    $scope.audio.pause();
+    $scope.playing = false;
+  };
+  $scope.audio.addEventListener('ended', function() {
+    $scope.$apply(function() {
+      $scope.stop()
+    });
+  });
+}]);
+
+// Keeps track of audio element and fetches NPR program listings
+
+app.controller('RelatedController', ['$scope', function($scope) {
+
+}]);
